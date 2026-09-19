e2216d624ff90ad70d2ccc9c2fd8b63ad5e92eb7
qlnx2012 <qlnx2012@user.noreply.gitee.com>
2026-09-19 22:25:48 +0800

    port: adapt NeoForge 1.21.1
    (cherry picked from commit 97226b9732273b94fb0fe1c5ed57d0cf1d258cc7)

--- src/main/java/com/qshop/sellupgrade/QShopSellUpgradeMod.java
+++ src/main/java/com/qshop/sellupgrade/QShopSellUpgradeMod.java
@@ -3,9 +3,10 @@ package com.qshop.sellupgrade;
 import com.qshop.sellupgrade.events.PickupContextEvents;
 import com.qshop.sellupgrade.init.ModItems;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(QShopSellUpgradeMod.MOD_ID)
 public final class QShopSellUpgradeMod {
@@ -13,10 +14,9 @@ public final class QShopSellUpgradeMod {
     public static final ResourceLocation AUTO_SELL_SOURCE =
             ResourceLocation.fromNamespaceAndPath(MOD_ID, "auto_sell");
 
-    public QShopSellUpgradeMod() {
-        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public QShopSellUpgradeMod(IEventBus modEventBus, ModContainer modContainer) {
         ModItems.ITEMS.register(modEventBus);
         modEventBus.addListener(ModItems::registerContainers);
-        MinecraftForge.EVENT_BUS.register(new PickupContextEvents());
+        NeoForge.EVENT_BUS.register(new PickupContextEvents());
     }
 }
